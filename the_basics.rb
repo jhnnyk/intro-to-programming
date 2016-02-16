654ec58e3ce28233d2477baba5769551d8f3e60f
John Knotts <jhnnyk@gmail.com>
2016-02-15 18:04:53 -0700

    Chapter 1: exercises 1 & 2

---
+++ the_basics.rb
@@ -0,0 +1,13 @@
+# Exercise 1
+puts "John " + "Knotts"
+
+# Exercise 2
+n = 9876
+thousands = n / 1000
+hundreds = n % 1000 / 100
+tens = n % 100 / 10
+ones = n % 10
+
+puts "#{thousands} thousands, #{hundreds} hundreds, #{tens} tens and #{ones} ones."
+
+# Exercise 3
